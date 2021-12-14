c479ad9bd5c645d3a0eb482017e812d54078f6c4
emrekaradag <aliemrekaradag1@gmail.com>
2021-12-14 07:27:03 +0300

    UserService deleteById , getAllUsers, and save methods created

--- src/main/java/com/issue/management/service/UserService.java
+++ src/main/java/com/issue/management/service/UserService.java
@@ -2,10 +2,15 @@ package com.issue.management.service;
 
 import com.issue.management.entity.User;
 import com.issue.management.entity.converter.UserEntitiyConverter;
+import com.issue.management.entity.dto.CreateUserRequest;
 import com.issue.management.entity.dto.UserDto;
 import com.issue.management.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,13 +19,34 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserEntitiyConverter userEntitiyConverter;
 
-    public UserDto createUser(User user) {
-        return userEntitiyConverter.toDto(userRepository.save(user));
+    public UserDto createUser(CreateUserRequest request) {
+        User user = new User();
+        user.setId(request.getId());
+        user.setUsername(request.getUserName());
+
+        userRepository.save(user);
+
+        return userEntitiyConverter.toDto(user);
     }
 
 
-    public User findUserById(Long userId){
-        return userRepository.findById(userId).orElseThrow(NoSuchFieldError::new);
+    public List<UserDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        List<UserDto> userDtos = new ArrayList<>();
+        users.forEach(user -> {
+            userDtos.add(userEntitiyConverter.toDto(user));
+        });
+        return userDtos;
+    }
+
+    public UserDto findUserById(Long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        return userEntitiyConverter.toDto(userOptional.get());
+    }
+
+    public void deleteUserById(Long id){
+        Optional<User> optionalUser = userRepository.findById(id);
+        userRepository.deleteById(optionalUser.get().getId());
     }
 
 }
